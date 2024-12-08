bc0d9e075072ab320a53ddff140fe71b6978f4ca
Chetna-Jha-QC <ms.chetnajha@gmail.com>
2024-12-08 18:42:59 +0530

    Add isPrime function to check if a number is prime

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+module.exports = { isPrime };  // Export the function to use it in other files
